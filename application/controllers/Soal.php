eb9572f8898fa659e9ef8c7e06de0a9987df3977
edward97 <edw.suryajaya@gmail.com>
2018-06-29 16:19:23 +0700

    revisi design dikit

--- application/controllers/Soal.php
+++ application/controllers/Soal.php
@@ -14,10 +14,12 @@ class Soal extends CI_Controller
 			redirect('login');
 		}
 		$this->load->model('soal_model');
+		$this->load->model('setting_model');
 	}
 
 	function input() {
 		if ($this->session->userdata('akses') ==  '1') {
+			$data['divisi'] = $this->setting_model->tampil_job()->result();
 			$data['format'] = mdate('%d-%M-%Y %H:%i %a', now('Asia/Jakarta'));
 			$data['judul'] = "Input Soal";
 
@@ -37,6 +39,24 @@ class Soal extends CI_Controller
 
 			$this->load->view('admin/v_menu', $data);
 			$this->load->view('admin/v_soal_list');
+			$this->load->view('admin/v_footer');
+		}
+		else {
+			$this->load->view('index.html');
+		}
+	}
+
+	function next() {
+		if ($this->session->userdata('akses') ==  '1' AND $this->input->post('submit') == "lanjut") {
+			$data['format'] = mdate('%d-%M-%Y %H:%i %a', now('Asia/Jakarta'));
+			$data['judul'] = "Kumpulan Soal";
+
+			$data['divisi'] = $this->input->post('divisi');
+			$data['tipe'] = $this->input->post('tipe_soal');
+			$data['label'] = $this->input->post('nama_soal');
+
+			$this->load->view('admin/v_menu', $data);
+			$this->load->view('admin/v_soal_next');
 			$this->load->view('admin/v_footer');
 		}
 		else {
--- application/views/admin/v_soal_input.php
+++ application/views/admin/v_soal_input.php
@@ -1,1 +1,52 @@
-<h3>input soal</h3>
+<div class="card-deck">
+	<div class="card">
+		<div class="card-body">
+			<h5 class="card-title">Input Soal</h5>
+			<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+
+			<?php echo form_open('soal/next') ?>
+				<div class="form-group row">
+					<label for="divisi" class="col-sm-2 col-form-label">Divisi</label>
+					
+					<div class="col-sm-4">
+						<select name="divisi" id="divisi" class="form-control">
+							<option selected>Choose...</option>
+							<?php foreach ($divisi as $i): ?>
+								<option value="<?php echo $i->id_job ?>"><?php echo $i->nm_job ?></option>
+							<?php endforeach ?>
+						</select>
+					</div>
+				</div>
+
+				<div class="form-group row">
+					<label for="tipe_soal" class="col-sm-2 col-form-label">Tipe Ujian</label>
+					
+					<div class="col-sm-4">
+						<select name="tipe_soal" id="tipe_soal" class="form-control">
+							<option selected>Choose...</option>
+							<option value="Ujian Online">Ujian Online</option>
+							<option value="Ujian Tatap Muka">Ujian Tatap Muka</option>
+						</select>
+					</div>
+				</div>
+
+				<div class="form-group row">
+					<label for="nama_soal" class="col-sm-2 col-form-label">Nama Soal</label>
+					
+					<div class="col-sm-4">
+						<input type="text" class="form-control" name="nama_soal" id="nama_soal">
+						<small id="helpSoal" class="form-text text-muted">[ Akuntansi I, Akuntansi II ]</small>
+					</div>
+				</div>
+
+				<div class="form-group row">
+					<label class="col-sm-2 col-form-label">&nbsp;</label>
+					
+					<div class="col-sm-4">
+						<button type="submit" name="submit" id="submit" class="btn btn-success" value="lanjut">Lanjut</button>
+					</div>
+				</div>
+			<?php echo form_close() ?>
+		</div>
+	</div>
+</div>
